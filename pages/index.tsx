e33bce1f28a176b5c6f29e132d6a6f3d2f13548f
Andrew Holloway <andrewholloway123@hotmail.com>
2023-02-05 17:40:37 +0000

    chapter 4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,65 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Nav from "../components/Nav.jsx"
-import { useState } from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Nav from "../components/Nav.jsx";
+import { useState, useEffect } from 'react';
+import * as fcl from "@onflow/fcl";
 
 export default function Home() {
+  const [greeting, setGreeting] = useState('');
   const [newGreeting, setNewGreeting] = useState('');
 
-  function runTransaction() {
-    console.log(newGreeting)
+  async function runTransaction() {
+    const transactionId = await fcl.mutate({
+      cadence: `
+      import HelloWorld from 0x741dac035a02ca4e
+  
+      transaction(myNewGreeting: String) {
+  
+        prepare(signer: AuthAccount) {}
+  
+        execute {
+          HelloWorld.changeGreeting(newGreeting: myNewGreeting)
+        }
+      }
+      `,
+      args: (arg, t) => [
+        arg(newGreeting, t.String)
+      ],
+      proposer: fcl.authz,
+      payer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 999
+    })
+  
+    console.log("Here is the transactionId: " + transactionId);
+    await fcl.tx(transactionId).onceSealed();
+    executeScript();
   }
+
+
+  async function executeScript() {
+    const response = await fcl.query({
+      cadence: ` import HelloWorld from 0x741dac035a02ca4e
+
+    pub fun main(): String {
+        return HelloWorld.greeting
+    }
+    `, 
+      args: (arg, t) => [] // ARGUMENTS GO IN HERE
+    })
+  
+    
+    setGreeting(response);
+  }
+
+  useEffect(() => {
+    executeScript()
+  }, [])
+
   function printGoodbye() {
     console.log("Goodbye!")
   }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -29,6 +77,7 @@ export default function Home() {
         <button onClick={runTransaction}>Run Transaction</button>
         <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Hello, Idiots!" />
         </div>
+        <p>{greeting}</p>
       </main>
     </div>
   )
